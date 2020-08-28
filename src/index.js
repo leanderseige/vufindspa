bc03d709d30b60907d02c661de69f9a8f64240e1
Leander Seige <leander@seige.name>
2020-08-28 17:15:44 +0200

    improved layout

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
